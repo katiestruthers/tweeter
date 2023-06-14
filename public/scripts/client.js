6ac6a6c5ca44c3f7b2e16a8f2181e8fbdd78c656
Katherine Struthers <ktstruthers@gmail.com>
2023-06-14 12:19:18 -0500

    create dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,66 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+/* eslint-disable no-undef */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+$(document).ready(function() {
+  const createTweetElement = function(tweet) {
+    let $tweet = $(`
+      <article class="tweet">
+      <header>
+        <span>
+          <img src="${tweet.user.avatars}">
+          <span>${tweet.user.name}</span>
+        </span>
+        <span>${tweet.user.handle}</span>
+      </header>
+      <div class="tweet-text">
+        <p>${tweet.content.text}</p>
+      </div>
+      <footer>
+        <span>${tweet.created_at}</span>
+        <span>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </span>
+      </footer>
+      </article>`);
+
+    return $tweet;
+  };
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  };
+
+  renderTweets(data);
+});
