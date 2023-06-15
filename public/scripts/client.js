3074f88a9ab4cb97f3c2b46d0e95650ea40ef218
Katherine Struthers <ktstruthers@gmail.com>
2023-06-15 10:13:06 -0500

    fetch data for tweets using AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 /* eslint-disable no-undef */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 $(document).ready(function() {
   const createTweetElement = function(tweet) {
     let $tweet = $(`
@@ -45,7 +20,7 @@ $(document).ready(function() {
         <p>${tweet.content.text}</p>
       </div>
       <footer>
-        <span>${tweet.created_at}</span>
+        <span>${timeago.format(tweet.created_at)}</span>
         <span>
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -64,7 +39,16 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
+  const loadTweets = function() {
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:8080/tweets'
+    }).then(function(data) {
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
 
   $('form').on('submit', function(event) {
     event.preventDefault();
