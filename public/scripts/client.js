b917ee73c873a0ab8af069a758ffa60db64ea620
Katherine Struthers <ktstruthers@gmail.com>
2023-06-23 05:14:49 -0500

    add styled error messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -60,14 +60,16 @@ $(document).ready(function() {
     event.preventDefault();
     const data = $(this).serialize();
 
+    $(".form-error").slideUp();
+
     // Validate correct form submission
     const dataArr = data.split('=');
     const value = dataArr[1];
     if (!value) {
-      alert('Cannot post an empty tweet.');
+      $("#form-error-empty").slideDown();
     }
     if (value.length > 140) {
-      alert('Tweet exceeds maximum character limit of 140.');
+      $("#form-error-too-long").slideDown();
     }
 
     $.ajax({
